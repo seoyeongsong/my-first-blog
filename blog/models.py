ec0c7761221434a00d3ebb9be9061d863c12d520
seoyeongsong <highhopes7@gmail.com>
2018-07-21 14:27:08 +0900

    Modified templates to display posts form database.

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     auther = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_data = models.DateTimeField(
+    created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
